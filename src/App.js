39c4d4859901ab783887b46827e3c791a974b712
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-15 16:49:22 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect } from "react";
 function App() {
+  let menu;
+  useEffect(() => {
+    menu = document.getElementById("menu");
+    Array.from(document.getElementsByClassName("menu-items"))
+      .forEach((item,index) => {
+        item.onmouseover = () =>{
+          menu.dataset.activeIndex = index;
+        }
+      });
+
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div id="menu">
+      <div id="menu-items">
+        <a href="/" className="menu-items">
+          Home
         </a>
-      </header>
+        <a href="/" className="menu-items">
+          Shop
+        </a>
+        <a href="/" className="menu-items">
+          About
+        </a>
+        <a href="/" className="menu-items">
+          Contact Us
+        </a>
+      </div>
+      <div id="menu-background-pattern"> </div>
+      <div id="menu-background-image"> </div>
     </div>
   );
 }
